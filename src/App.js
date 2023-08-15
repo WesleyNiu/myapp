b22e77da81b1c68f405b8f4c64db3d36e0f419c1
SalutePeople <nyf_0512@163.com>
2023-08-16 01:17:45 +0800

    分支变化

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Router from '../src/router'
+import Router from "../src/router";
 function App() {
   return (
     <div className="App">
       <span>创建新的分支nyf-----------</span>
-      <Router/>
+      <Router />
+      <span>分支从新开始----</span>
     </div>
   );
 }

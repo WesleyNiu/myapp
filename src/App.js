f668a98ea805d5783edc65590de96b0a3b34c969
SalutePeople <nyf_0512@163.com>
2023-08-16 00:10:40 +0800

    修改app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">这是测试用的工程</div>;
+  return <div className="App">
+    <span>
+      新的修改方式
+    </span>
+  </div>;
 }
 
 export default App;

62d6a0c6c52aba3d81ea3f7c43403e87a5467e0b
SalutePeople <nyf_0512@163.com>
2023-08-16 22:07:41 +0800

    路由修改

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Router() {
-  return <div>这是路由组件</div>;
+  return (
+    <div>
+      <div>这是路由组件</div>
+      <div>这是路由组件</div>
+    </div>
+  );
 }

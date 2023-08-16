82837a1c4030208c8a887548c1aae4e79c047812
SalutePeople <nyf_0512@163.com>
2023-08-16 22:14:09 +0800

    路由组件变基合并多个commit
    路由组件变基合并多个commit
    
    测试1
    
    测试3
    
    测试3
    
    测试2
    
    测试3
    
    测试0
    
    将多个提交组合在一起
    
    更新分支记录

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,7 +4,15 @@ export default function Router() {
   return (
     <div>
       <div>这是路由组件</div>
+      <div>这是路由组件1</div>
+      <div>这是路由组件2</div>
+      <div>这是路由组件3</div>
       <div>这是路由组件</div>
+      <br />
+      <span>1</span>
+      <span>2</span>
+      <span>3</span>
+      <span>测试3</span>
     </div>
   );
 }
